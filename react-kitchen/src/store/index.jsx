d75845d1e41809f6805dcbf0f3b1549b4fec3442
Eugene Kofman <eugenekofman@gmail.com>
2022-12-09 18:05:38 -0500

    complete orders reducer

--- react-kitchen/src/store/index.jsx
+++ react-kitchen/src/store/index.jsx
@@ -1,23 +1,24 @@
-// import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-// import { logger } from 'redux-logger'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { logger } from 'redux-logger'
+import ordersReducer from './orders';
 
-// const rootReducer = combineReducers({
+const rootReducer = combineReducers({
+    orders: ordersReducer
+})
 
-// })
+let enhancer;
 
-// let enhancer;
-
-// if (process.env.NODE_ENV === 'production'){
-//     enhancer = applyMiddleware(thunk)
-// } else {
-//     // const logger = require('redux-logger').default;
+if (process.env.NODE_ENV === 'production'){
+    enhancer = applyMiddleware(thunk, logger)
+} else {
+    // const logger = require('redux-logger').default;
     
-//     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//     enhancer = composeEnhancers(applyMiddleware(thunk));
-// }
-// const configureStore = (preloadedState) => {
-//     return createStore(rootReducer, preloadedState, enhancer);
-// }
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+}
 
-// export default configureStore;
+export default configureStore;
--- react-kitchen/src/store/orders.jsx
+++ react-kitchen/src/store/orders.jsx
@@ -1,12 +1,73 @@
-// const RECEIVE_ORDERS = 'orders/RECEIVE_ORDERS'
-// const RECEIVE_ORDER = 'order/RECEIVE_ORDER'
+const RECEIVE_ORDERS = 'orders/RECEIVE_ORDERS'
+const RECEIVE_ORDER = 'order/RECEIVE_ORDER'
+const REMOVE_ORDER = 'orderId/DELETE_ORDER'
 
-// const receiveOrders = orders => ({
-//     type: RECEIVE_ORDERS,
-//     orders
-// })
+const receiveOrders = () => ({
+    type: RECEIVE_ORDERS,
+    orders
+})
 
-// const receiveOrder = order => ({
-//     type: RECEIVE_ORDER,
-//     order
-// })
+const receiveOrder = order => ({
+    type: RECEIVE_ORDER,
+    order
+})
+
+const removeOrder = orderId => ({
+    type: REMOVE_ORDER,
+    orderId
+})
+
+export const getOrders = () => ({ orders } ) => orders ? Object.values(orders) : null
+export const getOrder = orderId => ({ orders } ) => orders ? orders[orderId] : null
+
+export const fetchOrders = async dispatch => {
+    const res = await fetch('api/orders')
+    const data = await res.json()
+    dispatch(receiveOrders(data))
+
+}
+
+export const updateOrder = (order) => async dispatch => {
+    const res = await fetch(`api/order/${order.id}`, {
+        method: "patch",
+        body: order,
+        header: "application/json",
+
+    })
+    dispatch(fetchOrders)
+
+}
+
+export const createOrder = (order) => async dispatch => {
+    const res = await fetch('api/orders', {
+        method: "post",
+        header: "application/json",
+        body: JSON.stringify(order)
+    })
+    data = res.json()
+    dispatch(fetchOrders(data))
+}
+
+export const deleteOrder = orderId => async dispatch => {
+    const res = await fetch(`api/orders/${orderId}`, {
+        method: "DELETE",
+        header: "application/json"
+    });
+    // const data = await res.json()
+    dispatch(removeOrder(data))
+}
+
+const ordersReducer = (state = {}, action) => {
+    const nextState = { ...state }
+    switch(action.type) {
+        case RECEIVE_ORDERS:
+            return { ...nextState, ...action.orders }
+        case RECEIVE_ORDER:
+            delete nextState[orderId]
+            return nextState
+        default:
+            return state;
+    }
+}
+
+export default ordersReducer
